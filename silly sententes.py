2b4a3327b60522159ab14c67e38b92a4141dc031
leo <leo.iudakov@gmail.com>
2017-01-05 21:28:14 -0800

    printing random sentences

---
+++ silly sententes.py
@@ -0,0 +1,13 @@
+from random import randint
+from time import sleep
+name = ['Neha', 'Lee', 'Sam']
+verb = ['buys', 'rides', 'kicks']
+noun = ['lion', 'bicycle', 'plane']
+def pick(words):
+    num_words = len(words)
+    num_picked = randint(0, num_words - 1)
+    word_picked = words[num_picked]
+    return word_picked
+while True:
+    print(pick(name), pick(verb), 'a', pick(noun), end='.\n')
+    sleep(1)
